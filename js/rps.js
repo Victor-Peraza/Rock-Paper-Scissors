16fec022d1ff57f9dffe002dbc3a53f5e29d0789
Victor Peraza <perazav@outlook.com>
2022-10-15 02:57:51 -0700

    Connected most logic to UI

--- js/rps.js
+++ js/rps.js
@@ -5,53 +5,61 @@ let getComputerChoice = function () {
   return random;
 };
 
-var cpuScore = 0;
-var playerScore = 0;
+let cpuScore = 0;
+let playerScore = 0;
+let winOrLose = "";
 
 //Checks what beats what and returns winner or loser
 function playRound(playerSelection, computerSelection) {
-  playerSelection =
-    playerSelection.charAt(0).toUpperCase() + playerSelection.slice(1);
-
-  if (playerSelection == "Rock" || playerSelection == "Paper" || playerSelection == "Scissors") {
-
-    if (playerSelection == computerSelection) {
-      return "It's a Tie!";
-    } else if (playerSelection == "Rock" && computerSelection == "Scissors") {
-      ++playerScore;
-      return "You Win! " + playerSelection + " beats " + computerSelection;
-    } else if (playerSelection == "Paper" && computerSelection == "Rock") {
-      ++playerScore;
-      return "You Win! " + playerSelection + " beats " + computerSelection;
-    } else if (playerSelection == "Scissors" && computerSelection == "Paper") {
-      ++playerScore;
-      return "You Win! " + playerSelection + " beats " + computerSelection;
-    } else {
-      ++cpuScore;
-      return "You Lose! " + computerSelection + " beats " + playerSelection;
-    }
+  if (playerSelection == computerSelection) {
+    winOrLose = "Tie!";
+    return playerSelection + " ties with " + computerSelection;
+  } else if (playerSelection == "Rock" && computerSelection == "Scissors") {
+    ++playerScore;
+    winOrLose = "Winner!";
+    return playerSelection + " beats " + computerSelection;
+  } else if (playerSelection == "Paper" && computerSelection == "Rock") {
+    ++playerScore;
+    winOrLose = "Winner!";
+    return playerSelection + " beats " + computerSelection;
+  } else if (playerSelection == "Scissors" && computerSelection == "Paper") {
+    ++playerScore;
+    winOrLose = "Winner!";
+    return playerSelection + " beats " + computerSelection;
   } else {
-    return "Invalid Input. Nobody Wins."
+    ++cpuScore;
+    winOrLose = "Loser!";
+    return computerSelection + " beats " + playerSelection;
   }
 }
 
-//driver
-function game() {
-  for (let i = 1; i <= 5; i++) {
-    let playerSelection = prompt("pick").toLowerCase();
-    const computerSelection = getComputerChoice();
-    console.log("CPU:"+computerSelection);
-    console.log("Player: " + playerSelection);
-    console.log(playRound(playerSelection, computerSelection));
-    console.log("CPU Score: " + cpuScore);
-    console.log("Player Score: " + playerScore);
-  }
-  if (playerScore > cpuScore) {
-    console.log("Player Wins!");
-  } else {
-    console.log("CPU Wins...");
-  }
-}
+const container = document.querySelector("#container");
+const result = document.querySelector("#result");
+const winnerOrLoser = document.querySelector("#winnerOrLoser");
 
-//driver
-game();
+const rockButton = document.querySelector("#rock");
+rockButton.addEventListener("click", () => {
+  playRound("Rock", getComputerChoice());
+  result.textContent = playRound("Rock", getComputerChoice());
+  winnerOrLoser.textContent = winOrLose;
+  console.log(winOrLose);
+  console.log(result);
+});
+
+const paperButton = document.querySelector("#paper");
+paperButton.addEventListener("click", () => {
+  playRound("Paper", getComputerChoice());
+  result.textContent = playRound("Paper", getComputerChoice());
+  winnerOrLoser .textContent = winOrLose;
+  console.log(winOrLose);
+  console.log(result);
+});
+
+const scissorsButton = document.querySelector("#scissors");
+scissorsButton.addEventListener("click", () => {
+  playRound("Scissors", getComputerChoice());
+  result.textContent = playRound("Scissors", getComputerChoice());
+  winnerOrLoser .textContent = winOrLose;
+  console.log(winOrLose);
+  console.log(result);
+});
